fa2dd87a814a07b660dca9c3adf9847813344834
forzoreugh <forzoreugh@gmail.com>
2025-05-05 00:03:51 +0300

    Completing homework №6.

---
+++ src/test/java/AddItemInBasketTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AddItemInBasketTest extends BaseTest {
+
+    @Test
+    public void addItemInBasketTest() {
+        login();
+        String title = driver.findElement(By.cssSelector(".title ")).getText();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(title, "Products", "Логин не выполнен");
+        String addText = driver.findElement(By.xpath
+                ("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).getText();
+        softAssert.assertEquals(addText, "Add to cart", "Неверное наименование " +
+                "кнопки добавления товара.");
+        driver.findElement(By.cssSelector(".btn.btn_primary.btn_small.btn_inventory ")).click();
+        String removeText = driver.findElement(By.xpath
+                ("//*[@id=\"remove-sauce-labs-backpack\"]")).getText();
+        softAssert.assertEquals(removeText, "Remove", "Неверное наименование кнопки удаления товара");
+        String nameProduct = driver.findElement(By.xpath("//*[@id=\"item_1_title_link\"]/div")).getText();
+        String countProduct = driver.findElement(By.xpath
+                ("//*[@id=\"inventory_container\"]/div/div[3]/div[2]/div[2]/div")).getText();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        String payNameProduct = driver.findElement(By.cssSelector(".inventory_item_name")).getText();
+        String payCountProduct = driver.findElement(By.cssSelector(".inventory_item_price")).getText();
+        softAssert.assertEquals(nameProduct, payNameProduct);
+        softAssert.assertEquals(countProduct, payCountProduct);
+    }
+}
