3ea87fb392b66b4f21853d776bb6fbc5f3c5542e
forzoreugh <forzoreugh@gmail.com>
2025-06-13 21:51:03 +0300

    Delete dataprovider from LoginTest[secrets.user, secrets.password]. Fix browser [SauceDemo]

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -62,18 +62,4 @@ public class LoginTest extends BaseTest {
                 .login("", "");
         loginPage.assertErrorMessageUsername();
     }
-
-    /*
-    @DataProvider
-    public Object[][] loginData() {
-        return new Object[][]{
-                {"standard_user", "secret_sauce"}
-                //               {"locked_out_user", "secret_sauce"},
-                //               {"problem_user", "secret_sauce"},
-                //              {"performance_glitch_user", "secret_sauce"},
-                //              {"error_user", "secret_sauce"},
-                //              {"visual_user", "secret_sauce"},
-        };
-    }
-    */
 }
