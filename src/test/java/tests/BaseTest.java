a1cbe15a15c58f51d69f341fed0d6329d8e22d86
forzoreugh <forzoreugh@gmail.com>
2025-06-16 20:24:22 +0300

    add options to baseTest.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -42,6 +42,9 @@ public class BaseTest {
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
             options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.manage().window().maximize();
